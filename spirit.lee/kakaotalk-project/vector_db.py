239ac26ab94b2890c777dad919a134c990e6d588
inspirit941 <inspirit941@gmail.com>
2023-12-12 18:36:23 +0900

    fix: 500이 최선

--- spirit.lee/kakaotalk-project/vector_db.py
+++ spirit.lee/kakaotalk-project/vector_db.py
@@ -11,7 +11,7 @@ def init(files: list):
     for file_name in files:
         with open(file_name) as f:
             text += f.read()
-    docs = embeddings(text, 700)
+    docs = embeddings(text, 500)
     return docs
 
 
